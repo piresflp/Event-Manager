5a71d7316ac1a8c4477b6ba8160912124d515fa1
Felipe Pires <flpiresa@gmail.com>
2021-05-15 12:58:08 -0300

    Carregando eventos organizados pelo usuario

--- lib/pages/YourEvents.dart
+++ lib/pages/YourEvents.dart
@@ -1,4 +1,6 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 
 class YourEvents extends StatefulWidget {
@@ -8,136 +10,112 @@ class YourEvents extends StatefulWidget {
 
 class _YourEventsState extends State<YourEvents> {
   @override
+  void initState() {
+    super.initState();
+
+    Firebase.initializeApp().whenComplete(() {
+      setState(() {});
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Container(
-            width: double.infinity,
-            decoration: BoxDecoration(
-              color: Color(0xFFF5F4EF),
-              image: DecorationImage(
-                image: AssetImage("assets/images/ux_big.png"),
-                alignment: Alignment.topRight,
-              ),
-            ),
-            child: Column(
-              children: <Widget>[
-                Padding(
-                    padding: EdgeInsets.only(left: 20, top: 50, right: 20),
-                    child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: <Widget>[
-                          Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                            children: <Widget>[
-                              SvgPicture.asset("assets/icons/arrow-left.svg"),
-                              SvgPicture.asset("assets/icons/more-vertical.svg")
-                            ],
-                          ),
-                          SizedBox(height: 30),
-                          Text(
-                            "Seus Eventos",
-                            style: TextStyle(
-                              fontSize: 28,
-                              color: Color(0xFF0D1333),
-                              fontWeight: FontWeight.bold,
-                            ),
-                          ),
-                          SizedBox(height: 16),
-                          Row(children: <Widget>[
-                            SvgPicture.asset("assets/icons/person.svg"),
-                            SizedBox(width: 5),
-                            Text("#12312"),
-                            SizedBox(width: 20),
-                            SvgPicture.asset("assets/icons/star.svg"),
-                            SizedBox(width: 5),
-                            Text("5")
-                          ]),
-                        ])),
-                SizedBox(height: 60),
-                Expanded(
-                  child: Container(
-                    width: double.infinity,
-                    decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(50),
-                        color: Colors.white),
-                    child: Stack(
-                      children: <Widget>[
-                        Padding(
-                          padding: const EdgeInsets.all(30),
-                          child: Column(
+        body: SafeArea(
+            child: Container(
+                width: double.infinity,
+                decoration: BoxDecoration(
+                  color: Color(0xFFF5F4EF),
+                  image: DecorationImage(
+                    image: AssetImage("assets/images/ux_big.png"),
+                    alignment: Alignment.topRight,
+                  ),
+                ),
+                child: Column(
+                  children: <Widget>[
+                    Padding(
+                        padding: EdgeInsets.only(left: 20, top: 20, right: 20),
+                        child: Column(
                             crossAxisAlignment: CrossAxisAlignment.start,
                             children: <Widget>[
-                              Text("Course content",
-                                  style: TextStyle(
-                                    fontSize: 20,
-                                    color: Color(0xFF0D1333),
-                                    fontWeight: FontWeight.bold,
-                                  )),
-                              SizedBox(height: 30),
-                              EventContent(
-                                number: "01",
-                                duration: 5.35,
-                                title: "Teste!!!",
-                                isDone: true,
-                              )
-                            ],
-                          ),
-                        ),
-                        Positioned(
-                          right: 0,
-                          left: 0,
-                          bottom: 0,
-                          child: Container(
-                              padding: EdgeInsets.all(20),
-                              height: 100,
-                              width: double.infinity,
-                              decoration: BoxDecoration(
-                                  color: Colors.white,
-                                  borderRadius: BorderRadius.circular(40),
-                                  boxShadow: [
-                                    BoxShadow(
-                                        offset: Offset(0, 4),
-                                        blurRadius: 50,
-                                        color:
-                                            Color(0xFF0D1333).withOpacity(.1))
-                                  ]),
-                              child: Row(
+                              Row(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceBetween,
                                 children: <Widget>[
-                                  Container(
-                                    padding: EdgeInsets.all(14),
-                                    height: 56,
-                                    width: 80,
-                                    decoration: BoxDecoration(
-                                        color: Color(0xFFFFEDEE),
-                                        borderRadius:
-                                            BorderRadius.circular(40)),
-                                    child: SvgPicture.asset(
-                                        "assets/icons/shopping-bag.svg"),
-                                  )
+                                  SvgPicture.asset(
+                                      "assets/icons/arrow-left.svg")
                                 ],
-                              )),
-                        )
-                      ],
-                    ),
-                  ),
-                )
-              ],
-            )));
+                              ),
+                              SizedBox(height: 25),
+                              Text(
+                                "Seus Eventos",
+                                style: TextStyle(
+                                  fontSize: 28,
+                                  color: Color(0xFF0D1333),
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                            ])),
+                    SizedBox(height: 20),
+                    Expanded(
+                      child: Container(
+                        width: double.infinity,
+                        decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(50),
+                            color: Colors.white),
+                        child: Stack(
+                          children: <Widget>[
+                            Padding(
+                              padding: const EdgeInsets.all(30),
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: <Widget>[
+                                  Text("Organizados por você",
+                                      style: TextStyle(
+                                        fontSize: 22,
+                                        color: Color(0xFF0D1333),
+                                        fontWeight: FontWeight.bold,
+                                      )),
+                                  SizedBox(height: 20),
+                                  listYourEvents(),
+                                  SizedBox(height: 40),
+                                  Text("Próximos eventos",
+                                      style: TextStyle(
+                                        fontSize: 22,
+                                        color: Color(0xFF0D1333),
+                                        fontWeight: FontWeight.bold,
+                                      )),
+                                  SizedBox(height: 20),
+                                  EventContent(
+                                    numero: 1,
+                                    dia: "23/07/2003",
+                                    hora: "11:50",
+                                    nome: "Reuniao TCC",
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    )
+                  ],
+                ))));
   }
 }
 
 class EventContent extends StatelessWidget {
-  final String number;
-  final double duration;
-  final String title;
-  final bool isDone;
+  final int numero;
+  final String dia;
+  final String hora;
+  final String nome;
 
   const EventContent({
     Key? key,
-    this.number = "",
-    this.duration = 0,
-    this.title = "",
-    this.isDone = false,
+    this.numero = 0,
+    this.dia = "",
+    this.hora = "",
+    this.nome = "",
   }) : super(key: key);
 
   @override
@@ -146,7 +124,65 @@ class EventContent extends StatelessWidget {
       padding: const EdgeInsets.only(bottom: 30),
       child: Row(
         children: <Widget>[
-          Text(number,
+          Text(numero.toString(),
+              style: TextStyle(
+                fontSize: 32,
+                color: Color(0xFF0D1333).withOpacity(.5),
+                fontWeight: FontWeight.bold,
+              )),
+          SizedBox(width: 20),
+          Container(
+            child: RichText(
+                text: TextSpan(children: [
+              TextSpan(
+                  text: "$dia - $hora\n",
+                  style: TextStyle(
+                      color: Color(0xFF0D1333).withOpacity(.5), fontSize: 18)),
+              TextSpan(
+                  text: nome,
+                  style: TextStyle(
+                      fontSize: 18,
+                      color: Color(0xFF0D1333),
+                      fontWeight: FontWeight.w600,
+                      height: 1.5))
+            ])),
+          ),
+          Spacer(),
+          Container(
+            margin: EdgeInsets.only(left: 8),
+            height: 40,
+            width: 40,
+            decoration:
+                BoxDecoration(shape: BoxShape.circle, color: Color(0xFF49CC96)),
+            child: Icon(Icons.search, color: Colors.white),
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class AdmEventContent extends StatelessWidget {
+  final int numero;
+  final String dia;
+  final String hora;
+  final String nome;
+
+  const AdmEventContent({
+    Key? key,
+    this.numero = 0,
+    this.dia = "",
+    this.hora = "",
+    this.nome = "",
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(bottom: 30),
+      child: Row(
+        children: <Widget>[
+          Text(numero.toString(),
               style: TextStyle(
                 fontSize: 32,
                 color: Color(0xFF0D1333).withOpacity(.5),
@@ -156,11 +192,11 @@ class EventContent extends StatelessWidget {
           RichText(
               text: TextSpan(children: [
             TextSpan(
-                text: "$duration mins\n",
+                text: "$dia - $hora\n",
                 style: TextStyle(
                     color: Color(0xFF0D1333).withOpacity(.5), fontSize: 18)),
             TextSpan(
-                text: title,
+                text: nome,
                 style: TextStyle(
                     fontSize: 18,
                     color: Color(0xFF0D1333),
@@ -169,13 +205,20 @@ class EventContent extends StatelessWidget {
           ])),
           Spacer(),
           Container(
-            margin: EdgeInsets.only(left: 20),
+            margin: EdgeInsets.only(left: 10),
             height: 40,
             width: 40,
-            decoration: BoxDecoration(
-                shape: BoxShape.circle,
-                color: Color(0xFF49CC96).withOpacity(isDone ? 1 : 5)),
-            child: Icon(Icons.play_arrow, color: Colors.white),
+            decoration:
+                BoxDecoration(shape: BoxShape.circle, color: Color(0xFF49CC96)),
+            child: Icon(Icons.search, color: Colors.white),
+          ),
+          Container(
+            margin: EdgeInsets.only(left: 8),
+            height: 40,
+            width: 40,
+            decoration:
+                BoxDecoration(shape: BoxShape.circle, color: Color(0xFF49CC96)),
+            child: Icon(Icons.edit_outlined, color: Colors.white),
           )
         ],
       ),
@@ -183,21 +226,33 @@ class EventContent extends StatelessWidget {
   }
 }
 
-class BestSellerClipper extends CustomClipper<Path> {
-  @override
-  getClip(Size size) {
-    var path = Path();
-    path.lineTo(size.width - 20, 0);
-    path.lineTo(size.width, size.height / 2);
-    path.lineTo(size.width - 20, size.height);
-    path.lineTo(0, size.height);
-    path.lineTo(0, 0);
-    path.close();
-    return path;
-  }
-
-  @override
-  bool shouldReclip(covariant CustomClipper oldClipper) {
-    return false;
-  }
+Widget listYourEvents() {
+  String idUser = "JIL8fXU6qSO7ilMhyl6U0nbgvQk2"; // TODO: Pegar idUser
+  return StreamBuilder(
+      stream: FirebaseFirestore.instance
+          .collection("Eventos")
+          .where('idOrganizador', isEqualTo: idUser)
+          .snapshots(),
+      builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+        if (!snapshot.hasData) {
+          return RichText(
+            text: TextSpan(children: [
+              TextSpan(
+                  text:
+                      "Opa! Parece que você ainda não organiza nenhum evento :(")
+            ]),
+          );
+        } else {
+          return Column(
+              children: List.generate(snapshot.data!.docs.length, (index) {
+            dynamic evento = snapshot.data!.docs[index].data()!;
+            return AdmEventContent(
+              numero: (index + 1),
+              dia: evento["dia"],
+              hora: evento["hora"],
+              nome: evento["nome"],
+            );
+          }));
+        }
+      });
 }
