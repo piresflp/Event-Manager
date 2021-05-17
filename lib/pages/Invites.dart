22d4af99985771c7f8ccf20d9e8375b9dba4425b
oGabrielArruda <47468017+oGabrielArruda@users.noreply.github.com>
2021-05-17 19:12:13 -0300

    event local

--- lib/pages/Invites.dart
+++ lib/pages/Invites.dart
@@ -71,78 +71,93 @@ class _InvitesState extends State<Invites> {
       builder: (BuildContext context, AsyncSnapshot<dynamic> uData) {
         var evento = uData.data();
 
-        return Padding(
-          padding: const EdgeInsets.all(30.0),
-          child: Container(
-            decoration: BoxDecoration(
-                border: Border.all(color: Colors.blueAccent),
-                borderRadius: BorderRadius.circular(8)),
-            child: Row(
-              children: [
-                Expanded(
-                  child: Padding(
-                      padding: EdgeInsets.fromLTRB(20, 10, 50, 20),
-                      child: Container(
-                        child: Column(
-                          children: [
-                            Text(evento['nome'], style: customTitle),
-                            SizedBox(height: 20),
-                            Row(
-                              children: [
-                                Text(
-                                  evento['hora'],
-                                  style: TextStyle(color: Colors.blueGrey),
-                                ),
-                                SizedBox(width: 40),
-                                Text(
-                                  evento['dia'],
-                                  style: TextStyle(color: Colors.blueGrey),
-                                ),
-                              ],
-                            )
-                          ],
-                        ),
-                      )),
-                ),
-                Column(
-                  children: [
-                    GestureDetector(
-                      onTap: () {},
-                      child: Padding(
-                        padding: EdgeInsets.only(right: 15),
-                        child: Container(
-                            width: 40,
-                            child: FittedBox(
-                              child: Icon(
-                                Icons.check,
-                                color: Colors.green,
+        return FutureBuilder(
+            future: Api.eventData(evento['local']),
+            builder: (BuildContext ctx, AsyncSnapshot<dynamic> localData) {
+              var local = localData.data();
+
+              return Padding(
+                padding: const EdgeInsets.all(30.0),
+                child: Container(
+                  decoration: BoxDecoration(
+                      border: Border.all(color: Colors.blueAccent),
+                      borderRadius: BorderRadius.circular(8)),
+                  child: Row(
+                    children: [
+                      Expanded(
+                        child: Padding(
+                            padding: EdgeInsets.fromLTRB(20, 10, 50, 20),
+                            child: Container(
+                              child: Column(
+                                children: [
+                                  Text(evento['nome'], style: customTitle),
+                                  SizedBox(height: 20),
+                                  Row(
+                                    children: [
+                                      Text(
+                                        evento['hora'],
+                                        style:
+                                            TextStyle(color: Colors.blueGrey),
+                                      ),
+                                      SizedBox(width: 40),
+                                      Text(
+                                        evento['dia'],
+                                        style:
+                                            TextStyle(color: Colors.blueGrey),
+                                      ),
+                                    ],
+                                  ),
+                                  Padding(
+                                    padding: EdgeInsets.fromLTRB(0, 7, 0, 0),
+                                    child: Text(
+                                      local['Descricao'],
+                                      style: TextStyle(color: Colors.blueGrey),
+                                    ),
+                                  ),
+                                ],
                               ),
-                              fit: BoxFit.fill,
                             )),
                       ),
-                    ),
-                    SizedBox(height: 15),
-                    GestureDetector(
-                      onTap: () {},
-                      child: Padding(
-                        padding: EdgeInsets.only(right: 15),
-                        child: Container(
-                            width: 40,
-                            child: FittedBox(
-                              child: Icon(
-                                Icons.close,
-                                color: Colors.red,
-                              ),
-                              fit: BoxFit.fill,
-                            )),
+                      Column(
+                        children: [
+                          GestureDetector(
+                            onTap: () {},
+                            child: Padding(
+                              padding: EdgeInsets.only(right: 15),
+                              child: Container(
+                                  width: 40,
+                                  child: FittedBox(
+                                    child: Icon(
+                                      Icons.check,
+                                      color: Colors.green,
+                                    ),
+                                    fit: BoxFit.fill,
+                                  )),
+                            ),
+                          ),
+                          SizedBox(height: 15),
+                          GestureDetector(
+                            onTap: () {},
+                            child: Padding(
+                              padding: EdgeInsets.only(right: 15),
+                              child: Container(
+                                  width: 40,
+                                  child: FittedBox(
+                                    child: Icon(
+                                      Icons.close,
+                                      color: Colors.red,
+                                    ),
+                                    fit: BoxFit.fill,
+                                  )),
+                            ),
+                          )
+                        ],
                       ),
-                    )
-                  ],
+                    ],
+                  ),
                 ),
-              ],
-            ),
-          ),
-        );
+              );
+            });
       },
     );
   }
